a01c870bc9114caf89c740ab53f506e4dbca8481
Watila Costa <watilafreire26@gmail.com>
2023-10-16 00:20:36 -0300

    first tests implements with adafruit pwm servo lib

--- spider-robot.ino
+++ spider-robot.ino
@@ -1,31 +1,57 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-Servo frontLFemur; // 0
-Servo frontLTibia; // 5
-Servo frontRFemur; // 3
-Servo frontRTibia; // 2
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 
-Servo backLFemur; // 1
-Servo backLTibia; // 6
-Servo backRFemur; // 4
-Servo backRTibia; // 7
+#define MIN_PULSE_WIDTH 150
+#define MAX_PULSE_WIDTH 600
+#define DEFAULT_PULSE_WIDTH 150
+#define FREQUENCY 60
 
+const int tibiaFrontalE = 5;
+const int tibiaFrontalD = 2;
+const int tibiaTraseiraE = 6;
+const int tibiaTraseiraD = 7;
+
+const int femurFrontalE = 0;
+const int femurFrontalD = 3;
+const int femurTraseiroE = 1;
+const int femurTraseiroD = 4;
+
+const int tibiasCount = 4;
+const int femursCount = 4;
+
+int pulseWidth(int angle) {
+  int pulse_wide, analog_value;
+
+  pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  analog_value = int(float (pulse_wide) / 1000000 * FREQUENCY * 4096);
+
+  return analog_value;
+}
 
 void setup() {
-  frontLFemur.attach(0);
-  frontLTibia.attach(5);
-  frontRFemur.attach(3);
-  frontRTibia.attach(2);
+  Serial.begin(9600);
 
-  backLFemur.attach(1);
-  backLTibia.attach(6);
-  backRFemur.attach(4);
-  backRTibia.attach(7);
+  pwm.begin();
+  pwm.setPWMFreq(FREQUENCY);
+  delay(1000);
+
+  pwm.setPWM(tibiaFrontalE, 0, pulseWidth(90));
+  pwm.setPWM(tibiaFrontalD, 0, pulseWidth(90));
+  pwm.setPWM(tibiaTraseiraE, 0, pulseWidth(90));
+  pwm.setPWM(tibiaTraseiraD, 0, pulseWidth(90));
 }
 
 void loop() {
-  frontLFemur.write(90);
-  frontLTibia.write(90);
-  frontRFemur.write(90);
-  frontRTibia.write(90);
+  delay(1000);
+  pwm.setPWM(tibiaFrontalE, 0, pulseWidth(180));
+  pwm.setPWM(tibiaFrontalD, 0, pulseWidth(180));
+  pwm.setPWM(tibiaTraseiraE, 0, pulseWidth(180));
+  pwm.setPWM(tibiaTraseiraD, 0, pulseWidth(180));
+  delay(1000);
+  pwm.setPWM(tibiaFrontalE, 0, pulseWidth(90));
+  pwm.setPWM(tibiaFrontalD, 0, pulseWidth(90));
+  pwm.setPWM(tibiaTraseiraE, 0, pulseWidth(90));
+  pwm.setPWM(tibiaTraseiraD, 0, pulseWidth(90));
 }
